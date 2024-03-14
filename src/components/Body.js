3f6d9078bfa00f961bd7a77a7a1760e68ecce3b6
Bhavanasrinivasa <bhavanasrinivasa27@gmail.com>
2024-03-14 17:49:11 +0530

    filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,31 @@
 import RestCard from "./RestCard";
+import { useState } from "react";
 import resData from "../utils/sampleData";
 
 const Body = () => {
+  const [listRes, setListRes] = useState(resData);
+
   return (
     <div className="body">
       <div className="search">
         <input className="input" type="text"></input>
       </div>
+      <div>
+        <button
+          className="fil-btn"
+          onClick={() => {
+            const filteredList = listRes.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            console.log(filteredList);
+            setListRes(filteredList);
+          }}
+        >
+          Top rated restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resData.map((restaurant) => (
+        {listRes.map((restaurant) => (
           <RestCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
